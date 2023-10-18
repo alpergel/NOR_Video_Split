29f26e176ecab0e52c9cc8d411a39e42b020480e
alpergel <aalisveris04@gmail.com>
2023-10-18 12:11:32 -0700

    fix sequence error

--- split.py
+++ split.py
@@ -2,14 +2,23 @@ import os
 import subprocess
 import argparse
 
-def split_video(filename, segment_size_mb=50):
+
+def get_length(filename):
+    result = subprocess.run(['C:\\Users\\aalis\\Documents\\Code\\ConboyLab\\ffmpeg\\ffmpeg-2023-10-12-git-a7663c9604-essentials_build\\bin\\ffprobe.exe', "-v", "error", "-show_entries",
+                             "format=duration", "-of",
+                             "default=noprint_wrappers=1:nokey=1", filename],
+    stdout=subprocess.PIPE,
+    stderr=subprocess.STDOUT)
+    return float(result.stdout)
+def split_video(filename, segment_size_mb=25):
     # Calculate segment size in bytes
     segment_size_bytes = segment_size_mb * 1024 * 1024
 
     # Check the file size
     filesize_bytes = os.path.getsize(filename)
     number_of_segments = int(filesize_bytes / segment_size_bytes) + 1
-
+    total_duration = get_length(filename)
+    
     # Split the filename to get the base and the extension
     filebase, fileext = os.path.splitext(filename)
     file = os.path.basename(filename)
@@ -18,21 +27,33 @@ def split_video(filename, segment_size_mb=50):
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
     # Use ffmpeg to split the video
-    for i in range(number_of_segments):
-        start_time = i * segment_size_bytes / filesize_bytes
-        output_file = f"{output_dir}\\segment_{i + 1}{fileext}"
-    
+    # Initalize Start Time
+    start_time = 0.0
+    segment_number = 0
+    segment_duration_seconds = (segment_size_bytes / filesize_bytes) * total_duration
+    while True:
+        output_file = f"{output_dir}\\segment_{segment_number}{fileext}"
         command = [
             'C:\\Users\\aalis\\Documents\\Code\\ConboyLab\\ffmpeg\\ffmpeg-2023-10-12-git-a7663c9604-essentials_build\\bin\\ffmpeg.exe',
             '-i', filename,
             '-ss', str(start_time),
             '-c', 'copy',
-            '-fs', str(segment_size_bytes),
+            '-t', str(segment_duration_seconds),
             output_file
         ]
+        
+        if subprocess.call(command) != 0:
+            break
+        # Check if the file was created and if its size is small, break
+        if os.path.exists(output_file) and os.path.getsize(output_file) < 1000:  # Less than 1KB
+            os.remove(output_file)
+            break
+        # Update start_time for the next segment
+        start_time += segment_duration_seconds
+        segment_number += 1
+        print(f"Processing segment {segment_number} from {start_time}s to {start_time + segment_duration_seconds}s")
 
-        subprocess.call(command)
-
+        
     print(f"Video split into {number_of_segments} segments in the directory '{output_dir}'.")
     print(output_file)
 
